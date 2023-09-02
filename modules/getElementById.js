0dadaf34665617ced2e9aae8e770b8902925097a
Chopper <grinpis01@gmail.com>
2023-09-03 01:46:29 +0300

    localStorage

--- modules/getElementById.js
+++ modules/getElementById.js
@@ -10,6 +10,20 @@ export function getElement() {
         const authorUser = document.getElementById("authorUser");
         const btnReg = document.getElementById("btnReg");
         const appReg = document.getElementById("appReg");
+        const appElement = document.getElementById("app");
+        const sendUserLogin = document.getElementById("sendUserLogin");
+        const sendUserPassword = document.getElementById("sendUserPassword");
+        const sendUserDate = document.getElementById("sendUserDate");
+        const userRegForm = document.getElementById("userRegForm");
+        const userHideForm = document.getElementById("userHideForm");
+        const regElement = document.getElementById("appReg");
+        const userLogin = document.getElementById("userLogin");
+        const userName = document.getElementById("userLogin");
+        const userPassword = document.getElementById("userLogin");
+        const sendBtn = document.getElementById("sendBtn");
+        const goToReg = document.getElementById("goToReg");
+        const outBtn = document.getElementById("outBtn");
+
 
         const module = {
                 sendButton: sendButton,
@@ -21,7 +35,20 @@ export function getElement() {
                 comments: comments,
                 authorUser: authorUser,
                 btnReg: btnReg,
-                appReg: appReg
+                appReg: appReg,
+                appElement: appElement,
+                sendUserLogin: sendUserLogin,
+                sendUserPassword: sendUserPassword,
+                sendUserDate: sendUserDate,
+                userRegForm: userRegForm,
+                userHideForm: userHideForm,
+                regElement: regElement,
+                userLogin: userLogin,
+                userName: userName,
+                userPassword: userPassword,
+                sendBtn: sendBtn,
+                goToReg: goToReg,
+                outBtn: outBtn
         }
         
         return module
